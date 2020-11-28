3366502538bdaff5da7f28579754822db9bddf5a
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2020-11-29 00:30:12 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  
+    
+</head>
+<body>
+    <h1>Daniel Harazim</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-wilQ-1">github</a>
+    <br>
+    <a href="praciorg.php">Organizacja i Pracownicy</a>
+    <a href="funkAgregujaca.php">Funkcje agregujące</a>
+  <a href="DataiCzas.php">Data i Czas</a>
+  <a href="formularz.html">Formularz</a>
+    <a href="danedobazy.html">Dane do bazy</a>
+</body>
+</html>
+<?php
+require_once("connect.php");
+
+
+
+?>
+</body>
+</html>
