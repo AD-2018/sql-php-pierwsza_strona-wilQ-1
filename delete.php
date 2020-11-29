a5edad72f63e274af1ec48913d75ccc388edeae1
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2020-11-29 16:37:45 +0100

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,24 @@
+  
+<?php
+echo $_POST['id_pracownicy'];
+
+require "connect.php";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id_pracownicy'];
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "<br>Record deleted successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
