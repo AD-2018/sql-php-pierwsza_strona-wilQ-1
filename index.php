d0b4e75e45d9dbf397bba5fdae31e4449f5bf6f0
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2020-11-29 10:47:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Page Title</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  
+    
 </head>
 <body>
+    <h1>Bartosz Wilczek</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-wilQ-1">github</a>
+    <br>
+    <a href="pracownicyiorganizacja.php">Organizacja i Pracownicy</a>
+    <a href="funkAgregujaca.php">Funkcje agregujące</a>
+  <a href="DataiCzas.php">Data i Czas</a>
+  <a href="formularz.html">Formularz</a>
+    <a href="danedobazy.html">Dane do bazy</a>
+</body>
+</html>
+<?php
+require_once("connect.php");
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
 
+
+?>
 </body>
 </html>
