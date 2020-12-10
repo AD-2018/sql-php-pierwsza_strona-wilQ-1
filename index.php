af5dab61d45b8d3f737466a4bf1168f52b981952
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2020-12-10 08:52:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,16 +13,10 @@
     <nav>
         <ul>
             <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-wilQ-1">github</a> </li>
-    <br>
-    <br>
-    <br>
             <li><a href="praciorg.php">Organizacja i Pracownicy</a></li>
             <li><a href="funkcje_agregujace.php">Funkcje agregujące</a></li>
-    
             <li><a href="Data_czas.php">Data i Czas</a></li>
-    
             <li><a href="formularz.html">Formularz</a></li>
-    
             <li><a href="danedobazy.html">Dodawanie i usuwanie</a></li>
 </nav>
 </ul>
