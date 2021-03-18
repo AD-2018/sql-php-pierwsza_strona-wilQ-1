06d6d7c1e027534d4d0f88e85749ef9b8ba0a3fc
BartoszWilczek <wilczek.bartosz99@gmail.com>
2021-03-18 14:03:35 +0100

    pl

---
+++ cssgrid/grid.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF=8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<title>css trening</title>
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<div class="container">
+    <header>
+        <h1>Wprowadzenie do CSS'a </h1>
+    </header>
+    <nav>
+    jakiś tekst
+    </nav>
+
+    <main>
+    coś tam
+    </main>
+
+    <aside>
+    jeszcze coś innego
+    </aside>
+
+    <footer>
+    i koniec
+    </footer>
+   </div>
+</body>
+</html>
