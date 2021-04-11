1afdf06f93192961868d3b168effebb927cfe4ea
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2021-04-11 17:13:25 +0200

    Update index.php

--- cssgrid/strona3/index.php
+++ cssgrid/strona3/index.php
@@ -8,13 +8,24 @@
     <title>CSS gride</title>
    <link rel="stylesheet" href="style.css">
    </head>  
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <a class="panel" href="/cssgrid/index.php">Wróć</a>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0: />
-    <meta http=equiv="X-UA-Compatible" content="ie=edge" />
-    <title>CSS gride</title>
-   <link rel="stylesheet" href="style.css">
-   </head>  
+    
+<body>
+    <div class="container">
+        <header>
+            <h1>CSS Grid</h1>
+        </header>
+        <nav>
+            1
+        </nav>
+        <main>
+            2
+        </main>
+        <aside>
+            3
+        </aside>
+        <footer>
+            4
+        </footer>
+    
+</body>
+</html>
