80fc57532fb3e69255eb80892339de1f96cf0b56
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2021-04-11 17:14:30 +0200

    Update index.php

--- cssgrid/strona4/index.php
+++ cssgrid/strona4/index.php
@@ -1,1 +1,60 @@
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <a class="panel" href="/cssgrid/index.php">Wróć</a>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0: />
+    <meta http=equiv="X-UA-Compatible" content="ie=edge" />
+    <title>CSS gride</title>
+   <link rel="stylesheet" href="style.css">
+   </head>  
+   <body>
+       <div class="container">
+        <header>
+            1
+        </header>
+        <nav>
+                             <h1>Strona 4</h1>
+        </nav>
+        <main>
+        3
+        </main>
+         <aside>
+           5
+      </aside>
+        <footer>
+           4
+        </footer>
+    </div>
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <a class="panel" href="/cssgrid/index.php">Wróć</a>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0: />
+    <meta http=equiv="X-UA-Compatible" content="ie=edge" />
+    <title>CSS gride</title>
+   <link rel="stylesheet" href="style.css">
+   </head>  
+   <body>
+       <div class="container">
+        <header>
+            1
+        </header>
+        <nav>
+          <h1>strona 4</h1>
+        </nav>
+        <main>
+        3
+        </main>
+         <aside>
+           5
+      </aside>
+        <footer>
+           4
+        </footer>
+    </div>
+</body>
+</html>
