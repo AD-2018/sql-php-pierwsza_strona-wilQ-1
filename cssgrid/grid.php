2694cd32d3ed0ac18320f8d9aa5bd68eb0a542ff
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2021-04-11 17:09:15 +0200

    Update grid.php

--- cssgrid/grid.php
+++ cssgrid/grid.php
@@ -13,7 +13,14 @@
         <a class="panel" href="/index.php">Wróć</a>
         <a class="panel" href="/cssgrid/strona1/grid.php">STRONA 1</a>
         <a class="panel" href="/cssgrid/strona2/grid.php">STRONA 2</a>
-        <a class="panel" href="/cssgrid/strona3/grid.php">STRONA 3</a>
+        <a class="panel" href="/cssgrid/strona3/index.php">STRONA 3</a>
+        <a class="panel" href="/cssgrid/strona4/index.php">STRONA 4</a>
+        <a class="panel" href="/cssgrid/strona5/index.php">STRONA 5</a>
+        <a class="panel" href="/cssgrid/strona6/index.php">STRONA 6</a>
+        <a class="panel" href="/cssgrid/strona7/index.php">STRONA 7</a>
+        <a class="panel" href="/cssgrid/strona8/index.php">STRONA 8</a>
+        <a class="panel" href="/cssgrid/strona9/index.php">STRONA 9</a>
+        <a class="panel" href="/cssgrid/strona10/index.php">STRONA 10</a>
         <br>
     </div>
     </body>
