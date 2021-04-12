eeac3b55e60dbee45224edceb24d58f111279c86
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2021-04-12 06:10:46 +0200

    Update index.php

--- cssgrid/strona6/index.php
+++ cssgrid/strona6/index.php
@@ -1,1 +1,22 @@
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <a class="panel" href="/cssgrid/index.php">Wróć</a>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0: />
+    <meta http=equiv="X-UA-Compatible" content="ie=edge" />
+    <title>CSS gride</title>
+   <link rel="stylesheet" href="style.css">
+   </head>  
+  
+<body>
+    <div class="g">
+        <div class="a">1</div>
+        <div class="b">2</div>
+        <div class="c">3</div>
+        <div class="d">4</div>
+        <div class="e">5</div>
+        <div class="f">6</div>   
+    </div>
+</body>
+</html>
