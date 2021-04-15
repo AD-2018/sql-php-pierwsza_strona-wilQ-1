c46b933963d4ee6eba6603870ae45e6f6c5b761f
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2021-04-15 13:47:39 +0200

    Update grid.php

--- cssgrid/strona2/grid.php
+++ cssgrid/strona2/grid.php
@@ -10,13 +10,81 @@
    </head>  
   
 <body>
-    <div class="g">
-        <div class="a">1</div>
-        <div class="b">2</div>
-        <div class="c">3</div>
-        <div class="d">4</div>
-        <div class="e">5</div>
-        <div class="f">6</div>
-    </div>
-</body>
+       <?php
+                    require_once("connect.php");
+                     
+                    echo("<br>select * from Autorzy<br>");
+                    $sql = "select * from Autorzy";
+
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                    } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+
+                    echo('<table border="1">');
+                    echo('<th>ID</th><th>Autor</th>');
+
+                    while($row=mysqli_fetch_assoc($result)){
+                    echo('<tr>');
+                    echo('<td>'.$row['ID'].'</td><td>'.$row['Autor'].'</td>');
+                    echo('</tr>');
+                    }
+
+                    echo('</table>');
+
+                ?>
+            </div>
+            <div class="ww">
+                1
+            </div>
+            <div class="qq">
+                <?php
+                    require_once("connect.php");
+
+                    echo("<br>select * from Tytul<br>");
+                    $sql = "select * from Tytul";
+
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                    } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+                    echo('<table border="1">');
+                    echo('<th>ID</th><th>Tytul</th>');
+                    while($row=mysqli_fetch_assoc($result)){
+                    echo('<tr>');
+                    echo('<td>'.$row['ID'].'</td><td>'.$row['Tytul'].'</td>');
+                    echo('</tr>');
+                    }
+                    echo('</table>');
+                ?>
+            </div>
+            <div class="ee">
+                4
+            </div>
+            <div class="yy">
+                <?php
+                    require_once("connect.php");
+
+                    echo("<br>select i_d, Autor, Tytul from Autorzy, Tytul where i_d=ID order by Autor asc;<br>");
+                    $sql = "select i_d, Autor, Tytul from Autorzy, Tytul where i_d=ID order by Autor asc;";
+
+                    $result = mysqli_query($conn, $sql);
+                    if ( $result) {
+                    } else {
+                    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                    }
+                    echo('<table border="1">');
+                    echo('<th>ID</th><th>Autor</th><th>Tytul</th');
+                    while($row=mysqli_fetch_assoc($result)){
+                    echo('<tr>');
+                    echo('<td>'.$row['i_d'].'</td><td>'.$row['Autor'].'</td><td>'.$row['Tytul'].'</td>');
+                    echo('</tr>');
+                    }
+                    echo('</table>');
+                ?>
+            </div>
+        </div>
+    </body>
 </html>
