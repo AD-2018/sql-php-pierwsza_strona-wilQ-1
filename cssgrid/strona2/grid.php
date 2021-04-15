2f4674988d01fcef0f0b4bc9e02779de7ce06f17
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2021-04-15 14:26:36 +0200

    Update grid.php

--- cssgrid/strona2/grid.php
+++ cssgrid/strona2/grid.php
@@ -8,24 +8,29 @@
         <div class="container">
             <div class="hh">
                 <?php
-                     require_once("connect.php");
-
-                    echo("<br>Podzespoly<br>");
-                    $sql = "SELECT * FROM podzespoly";
+                    require_once("connect.php");
+                     
+                    echo("<br>select * from Autorzy<br>");
+                    $sql = "select * from Autorzy";
 
                     $result = mysqli_query($conn, $sql);
                     if ( $result) {
                     } else {
                     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
                     }
+
                     echo('<table border="1">');
-                    echo('<th>id</th><th>podzesppol</th>');
+                    echo('<th>ID</th><th>Autor</th>');
+
                     while($row=mysqli_fetch_assoc($result)){
                     echo('<tr>');
-                    echo('<td>'.$row['id'].'</td><td>'.$row['podzespol'].'</td>');
+                    echo('<td>'.$row['ID'].'</td><td>'.$row['Autor'].'</td>');
                     echo('</tr>');
                     }
+
                     echo('</table>');
+
+
                 ?>
             </div>
             <div class="ee">
@@ -35,22 +40,23 @@
                 <?php
                     require_once("connect.php");
 
-                    echo("<br>PRODUCENT<br>");
-                    $sql = "SELECT * FROM producent";
-    
+                    echo("<br>select * from Tytul<br>");
+                    $sql = "select * from Tytul";
+
                     $result = mysqli_query($conn, $sql);
                     if ( $result) {
                     } else {
                     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
                     }
                     echo('<table border="1">');
-                    echo('<th>ID</th><th>producent</th>');
+                    echo('<th>ID</th><th>Tytul</th>');
                     while($row=mysqli_fetch_assoc($result)){
                     echo('<tr>');
-                    echo('<td>'.$row['numer'].'</td><td>'.$row['producent'].'</td>');
+                    echo('<td>'.$row['ID'].'</td><td>'.$row['Tytul'].'</td>');
                     echo('</tr>');
                     }
                     echo('</table>');
+
                 ?>
             </div>
             <div class="ww">
@@ -58,26 +64,24 @@
             </div>
             <div class="qq">
                 <?php
-                    require_once("connect.php");
+                    
+require_once("connect.php");
 
-                    echo("<br>Producent i Podzespol<br>");
-                    $sql = "SELECT producent, podzespol from podzespoly, producent where numer=Id order by producent";
+                    echo("<br>select i_d, Autor, Tytul from Autorzy, Tytul where i_d=ID order by Autor asc;<br>");
+                    $sql = "select i_d, Autor, Tytul from Autorzy, Tytul where i_d=ID order by Autor asc;";
 
                     $result = mysqli_query($conn, $sql);
                     if ( $result) {
                     } else {
                     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
                     }
-
                     echo('<table border="1">');
-                    echo('<th>ID</th><th>Producent</th><th>Podzespol</th>');
-
+                    echo('<th>ID</th><th>Autor</th><th>Tytul</th');
                     while($row=mysqli_fetch_assoc($result)){
                     echo('<tr>');
-                    echo('<td>'.$row['numer'].'</td><td>'.$row['producent'].'</td><td>'.$row['podzespol'].'</td>');
+                    echo('<td>'.$row['i_d'].'</td><td>'.$row['Autor'].'</td><td>'.$row['Tytul'].'</td>');
                     echo('</tr>');
                     }
-
                     echo('</table>');
 
                 ?>
