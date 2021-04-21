8f898d229f3773d89b7a058266997862046ab97b
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2021-04-21 17:24:21 +0200

    Create delnauczyciele.php

---
+++ cssgrid/strona1/delnauczyciele.php
@@ -0,0 +1,26 @@
+  <?php
+echo("usuwanie");
+echo $_POST['ID'];
+
+
+require_once("connect.php");
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "DELETE FROM Nauczyciele WHERE id_naucz=".$_POST['ID'];
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+    header ('Location:https://php-wilczek.herokuapp.com/cssgrid/strona1/grid.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
