7d1c9ae1773ea7db47b200c5c3ca74a1c597e368
Bartosz Wilczek <73635103+BartoszWilczek@users.noreply.github.com>
2021-04-21 17:08:23 +0200

    Update deltabela1.php

--- cssgrid/strona1/deltabela1.php
+++ cssgrid/strona1/deltabela1.php
@@ -5,5 +5,30 @@
     <title>Bartosz Wilczek</title>
 </head>
 <body>
+    <?php
+echo("usuwanie");
+echo $_POST['ID'];
+
+require_once("connect.php");
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "DELETE FROM Nauczyciele WHERE ID=".$_POST['ID'];
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+    header ('Location:https://php-wilczek.herokuapp.com/cssgrid/strona2/grid.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
   </body>
 </html>
